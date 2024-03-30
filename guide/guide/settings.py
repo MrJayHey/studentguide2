ff594f90a5f8ba4726d867b8054ec80b9e1b4339
Artem Yurkov <yurkov.artyom21@gmail.com>
2024-03-30 18:07:51 +0300

    Настройка дебага

--- guide/guide/settings.py
+++ guide/guide/settings.py
@@ -11,8 +11,8 @@ dotenv.load_dotenv(dotenv_path=dotenv_path, override=True)
 SECRET_KEY = 'django-insecure-$kkf%!jh+_t&ts^qnq3+(8_&y%t9hj(ay4%)gl#wk(gsk@a$l^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv('DEBUG', default='False') == 'True'
-DEBUG = 'True'
+DEBUG = os.getenv('DEBUG', default='True') == 'True'
+# DEBUG = 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(' ')
 
@@ -121,10 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = '/app/staticfiles/'
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
