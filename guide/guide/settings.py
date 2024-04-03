b6833ae41071744cb29a03b43edbbbe40207f883
Artem Yurkov <yurkov.artyom21@gmail.com>
2024-04-03 22:49:54 +0300

    Исправление статики

--- guide/guide/settings.py
+++ guide/guide/settings.py
@@ -124,8 +124,7 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = '/app/staticfiles/'
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
